69775ea3b2a9690f3811c6afabd86d795e75d0ec
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-05-30 20:45:29 +0200

    progress
    transition to dataset

---
+++ data.js
@@ -0,0 +1,42 @@
+
+
+plants=[
+
+    {
+        img:"images/monstera.jpg",
+        date:22/5/24,
+        titolo:Monstera,
+        descrizione:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.'      
+    },
+    {
+        img:"images/POTHOS.jpeg",
+        date:22/5/24,
+        titolo:Photos,
+        descrizione:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.'      
+    },
+    {
+        img:"images/ficus.jpg",
+        date:22/5/24,
+        titolo:Ficus,
+        descrizione:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.'      
+    },
+    {
+        img:"images/philodendro.avif",
+        date:22/5/24,
+        titolo:Philodendro,
+        descrizione:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.'      
+    },
+    {
+        img:"images/peperomia-raindrop.webp",
+        date:22/5/24,
+        titolo:Peperomia,
+        descrizione:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.'      
+    },
+    {
+        img:"images/sanseveria.jpg",
+        date:22/5/24,
+        titolo:Sanseveria,
+        descrizione:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.'      
+    },
+    
+]
