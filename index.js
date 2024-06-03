3fe1effc4da6c678391e5170fd09471d6d181654
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-06-03 18:14:43 +0200

    data button
    moved the blog to data array.
    css view more btn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import { plants } from "./data.js";
+const blogContainer = document.getElementById('blog-container')
 
-console.log(plants)
+//console.log(plants)
+
+function firstDisplayBlog (blogs){
+    let emptyHtml =''
+     blogs.forEach(function(blog){
+        emptyHtml += `<div class="blog">
+                    <img src='${blog.img}' alt="${blog.titolo} plant">
+                    <h3>${blog.date}</h3>
+                    <h2>${blog.titolo}</h2>
+                    <p>
+                        ${blog.descrizione}
+                    </p>
+                    <button>Read more</button>
+                </div>`
+    });
+    return emptyHtml
+}
+
+blogContainer.innerHTML= firstDisplayBlog(plants)
