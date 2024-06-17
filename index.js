68083988d2ee5bb686a48430d8613bcfa8c7d25a
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-06-17 12:39:21 +0200

    fixed homePage
    fixed view blog and button

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function firstDisplayBlog (){
     });
     return emptyHtml
 }
-
+/*
 blogContainer.innerHTML= firstDisplayBlog()
 
 loadMore.addEventListener('click', function(){
@@ -37,7 +37,67 @@ loadMore.addEventListener('click', function(){
     }
    
 })
+*/
+function checkMediaQuery() {
+    if (window.matchMedia('(max-width: 480px)').matches) {
 
+        displayedObjects = plants.slice(0, 3)
+        firstDisplayBlog ()
+        blogContainer.innerHTML= firstDisplayBlog()
+        loadMore.style.display = 'block'
+     
+    } else if (window.matchMedia('(min-width: 768px) and (max-width: 1200px)').matches) {
+
+        displayedObjects = plants.slice(0, 4)
+        firstDisplayBlog ()
+        blogContainer.innerHTML= firstDisplayBlog()
+        loadMore.style.display = 'block'
+
+    } else if(window.matchMedia('(min-width: 1280px)').matches){
+
+        displayedObjects = plants
+        firstDisplayBlog ()
+        blogContainer.innerHTML= firstDisplayBlog()
+        loadMore.style.display = 'none'
+
+    }
+}
+
+window.addEventListener('resize', checkMediaQuery);
+checkMediaQuery();
+
+loadMore.addEventListener('click', function(){
+    if (window.matchMedia('(max-width: 480px)').matches){
+
+        if(displayedObjects.length < plants.length){
+            displayedObjects = plants.slice(0, 6)
+            firstDisplayBlog ()
+            blogContainer.innerHTML= firstDisplayBlog()
+            loadMore.textContent = 'View Less'
+        } else {
+            displayedObjects = plants.slice(0, 3)
+            firstDisplayBlog ()
+            blogContainer.innerHTML= firstDisplayBlog()
+            loadMore.textContent = 'View More'
+        }
+
+    } else if (window.matchMedia('(min-width: 768px) and (max-width: 1200px)').matches){
+
+        if(displayedObjects.length < plants.length){
+            displayedObjects = plants
+            firstDisplayBlog ()
+            blogContainer.innerHTML= firstDisplayBlog()
+            loadMore.textContent = 'View Less'
+        } else{
+            displayedObjects = plants.slice(0, 4)
+            firstDisplayBlog ()
+            blogContainer.innerHTML= firstDisplayBlog()
+            loadMore.textContent = 'View More'
+        }
+    }
+})
+
+/*
 function checkMediaQuery() {
 
     if(window.matchMedia('(min-width: 768px) and (max-width: 1200px)').matches){
@@ -93,7 +153,7 @@ function checkMediaQuery() {
 
 window.addEventListener('resize', checkMediaQuery);
 checkMediaQuery();
-
+*/
 const mainBtn = document.getElementById('main');
 mainBtn.addEventListener('click', function() {
 window.location.href = 'blog.html';
